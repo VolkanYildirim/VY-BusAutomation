d7126edd6a31825083419c4f5bbd13042cfeb0da
Volkan Yıldırım <23458532+VolkanYildirim@users.noreply.github.com>
2026-04-26 08:10:46 +0300

    Add project files.

---
+++ C_Sharp_Form_Otobus_Otomasyonu/Form1.Designer.cs
@@ -0,0 +1,218 @@
+﻿namespace C_Sharp_Form_Otobus_Otomasyonu
+{
+    partial class Otobus_Otomasyon
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Otobus_Otomasyon));
+            this.grpbkoltuklar = new System.Windows.Forms.GroupBox();
+            this.grpMusteri = new System.Windows.Forms.GroupBox();
+            this.btnIptal = new System.Windows.Forms.Button();
+            this.btnTamam = new System.Windows.Forms.Button();
+            this.rdoErkek = new System.Windows.Forms.RadioButton();
+            this.rdoBayan = new System.Windows.Forms.RadioButton();
+            this.txtSoyad = new System.Windows.Forms.TextBox();
+            this.txtAd = new System.Windows.Forms.TextBox();
+            this.lblSoyad = new System.Windows.Forms.Label();
+            this.lblAd = new System.Windows.Forms.Label();
+            this.grpYolculistesi = new System.Windows.Forms.GroupBox();
+            this.lstYolculiste = new System.Windows.Forms.ListBox();
+            this.btnGoster = new System.Windows.Forms.Button();
+            this.grpMusteri.SuspendLayout();
+            this.grpYolculistesi.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // grpbkoltuklar
+            // 
+            this.grpbkoltuklar.Location = new System.Drawing.Point(54, 41);
+            this.grpbkoltuklar.Name = "grpbkoltuklar";
+            this.grpbkoltuklar.Size = new System.Drawing.Size(240, 554);
+            this.grpbkoltuklar.TabIndex = 0;
+            this.grpbkoltuklar.TabStop = false;
+            this.grpbkoltuklar.Text = "Koltuklar";
+            // 
+            // grpMusteri
+            // 
+            this.grpMusteri.Controls.Add(this.btnIptal);
+            this.grpMusteri.Controls.Add(this.btnTamam);
+            this.grpMusteri.Controls.Add(this.rdoErkek);
+            this.grpMusteri.Controls.Add(this.rdoBayan);
+            this.grpMusteri.Controls.Add(this.txtSoyad);
+            this.grpMusteri.Controls.Add(this.txtAd);
+            this.grpMusteri.Controls.Add(this.lblSoyad);
+            this.grpMusteri.Controls.Add(this.lblAd);
+            this.grpMusteri.Location = new System.Drawing.Point(385, 41);
+            this.grpMusteri.Name = "grpMusteri";
+            this.grpMusteri.Size = new System.Drawing.Size(228, 211);
+            this.grpMusteri.TabIndex = 1;
+            this.grpMusteri.TabStop = false;
+            this.grpMusteri.Text = "Müsteri Bilgi";
+            // 
+            // btnIptal
+            // 
+            this.btnIptal.Location = new System.Drawing.Point(123, 181);
+            this.btnIptal.Name = "btnIptal";
+            this.btnIptal.Size = new System.Drawing.Size(75, 23);
+            this.btnIptal.TabIndex = 7;
+            this.btnIptal.Text = "İptal";
+            this.btnIptal.UseVisualStyleBackColor = true;
+            this.btnIptal.Click += new System.EventHandler(this.btnIptal_Click);
+            // 
+            // btnTamam
+            // 
+            this.btnTamam.Location = new System.Drawing.Point(25, 182);
+            this.btnTamam.Name = "btnTamam";
+            this.btnTamam.Size = new System.Drawing.Size(79, 23);
+            this.btnTamam.TabIndex = 6;
+            this.btnTamam.Text = "Tamam";
+            this.btnTamam.UseVisualStyleBackColor = true;
+            this.btnTamam.Click += new System.EventHandler(this.btnTamam_Click);
+            // 
+            // rdoErkek
+            // 
+            this.rdoErkek.AutoSize = true;
+            this.rdoErkek.Location = new System.Drawing.Point(25, 141);
+            this.rdoErkek.Name = "rdoErkek";
+            this.rdoErkek.Size = new System.Drawing.Size(53, 17);
+            this.rdoErkek.TabIndex = 5;
+            this.rdoErkek.TabStop = true;
+            this.rdoErkek.Text = "Erkek";
+            this.rdoErkek.UseVisualStyleBackColor = true;
+            this.rdoErkek.CheckedChanged += new System.EventHandler(this.rdoErkek_CheckedChanged);
+            // 
+            // rdoBayan
+            // 
+            this.rdoBayan.AutoSize = true;
+            this.rdoBayan.Location = new System.Drawing.Point(25, 117);
+            this.rdoBayan.Name = "rdoBayan";
+            this.rdoBayan.Size = new System.Drawing.Size(55, 17);
+            this.rdoBayan.TabIndex = 4;
+            this.rdoBayan.TabStop = true;
+            this.rdoBayan.Text = "Bayan";
+            this.rdoBayan.UseVisualStyleBackColor = true;
+            this.rdoBayan.CheckedChanged += new System.EventHandler(this.rdoBayan_CheckedChanged);
+            // 
+            // txtSoyad
+            // 
+            this.txtSoyad.Location = new System.Drawing.Point(103, 70);
+            this.txtSoyad.Name = "txtSoyad";
+            this.txtSoyad.Size = new System.Drawing.Size(100, 20);
+            this.txtSoyad.TabIndex = 3;
+            // 
+            // txtAd
+            // 
+            this.txtAd.Location = new System.Drawing.Point(103, 47);
+            this.txtAd.Name = "txtAd";
+            this.txtAd.Size = new System.Drawing.Size(100, 20);
+            this.txtAd.TabIndex = 2;
+            // 
+            // lblSoyad
+            // 
+            this.lblSoyad.AutoSize = true;
+            this.lblSoyad.Location = new System.Drawing.Point(22, 73);
+            this.lblSoyad.Name = "lblSoyad";
+            this.lblSoyad.Size = new System.Drawing.Size(58, 13);
+            this.lblSoyad.TabIndex = 1;
+            this.lblSoyad.Text = "Soyadınız :";
+            // 
+            // lblAd
+            // 
+            this.lblAd.AutoSize = true;
+            this.lblAd.Location = new System.Drawing.Point(22, 50);
+            this.lblAd.Name = "lblAd";
+            this.lblAd.Size = new System.Drawing.Size(41, 13);
+            this.lblAd.TabIndex = 0;
+            this.lblAd.Text = "Adınız :";
+            // 
+            // grpYolculistesi
+            // 
+            this.grpYolculistesi.Controls.Add(this.lstYolculiste);
+            this.grpYolculistesi.Location = new System.Drawing.Point(317, 252);
+            this.grpYolculistesi.Name = "grpYolculistesi";
+            this.grpYolculistesi.Size = new System.Drawing.Size(362, 343);
+            this.grpYolculistesi.TabIndex = 1;
+            this.grpYolculistesi.TabStop = false;
+            this.grpYolculistesi.Text = "Yolcu Listesi";
+            // 
+            // lstYolculiste
+            // 
+            this.lstYolculiste.FormattingEnabled = true;
+            this.lstYolculiste.Location = new System.Drawing.Point(18, 35);
+            this.lstYolculiste.Name = "lstYolculiste";
+            this.lstYolculiste.Size = new System.Drawing.Size(327, 290);
+            this.lstYolculiste.TabIndex = 0;
+            // 
+            // btnGoster
+            // 
+            this.btnGoster.Location = new System.Drawing.Point(385, 13);
+            this.btnGoster.Name = "btnGoster";
+            this.btnGoster.Size = new System.Drawing.Size(228, 23);
+            this.btnGoster.TabIndex = 2;
+            this.btnGoster.Text = "Yolcu Listesini Göster";
+            this.btnGoster.UseVisualStyleBackColor = true;
+            this.btnGoster.Click += new System.EventHandler(this.btnGoster_Click);
+            // 
+            // Otobus_Otomasyon
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(690, 603);
+            this.Controls.Add(this.btnGoster);
+            this.Controls.Add(this.grpYolculistesi);
+            this.Controls.Add(this.grpMusteri);
+            this.Controls.Add(this.grpbkoltuklar);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Name = "Otobus_Otomasyon";
+            this.Text = "Yıldırım Otobüs Otomasyonu";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.grpMusteri.ResumeLayout(false);
+            this.grpMusteri.PerformLayout();
+            this.grpYolculistesi.ResumeLayout(false);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.GroupBox grpbkoltuklar;
+        private System.Windows.Forms.GroupBox grpMusteri;
+        private System.Windows.Forms.Label lblSoyad;
+        private System.Windows.Forms.Label lblAd;
+        private System.Windows.Forms.RadioButton rdoErkek;
+        private System.Windows.Forms.RadioButton rdoBayan;
+        private System.Windows.Forms.TextBox txtSoyad;
+        private System.Windows.Forms.TextBox txtAd;
+        private System.Windows.Forms.Button btnIptal;
+        private System.Windows.Forms.Button btnTamam;
+        private System.Windows.Forms.GroupBox grpYolculistesi;
+        private System.Windows.Forms.ListBox lstYolculiste;
+        private System.Windows.Forms.Button btnGoster;
+    }
+}
+
---
+++ C_Sharp_Form_Otobus_Otomasyonu/Form1.cs
@@ -0,0 +1,241 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+
+namespace C_Sharp_Form_Otobus_Otomasyonu
+{
+    public partial class Otobus_Otomasyon : Form
+    {
+        ArrayList koltuklar = new ArrayList();
+
+        TextBox t = new TextBox();
+
+        Form Form2 = new Form();
+
+        Button BasılanButon;
+
+        string[] yolcular = new string[100];
+
+        string[] koltukcins = new string[100];
+
+        int yolcuSayisi = 0,koltukSayisi=0;
+
+        public Otobus_Otomasyon()
+        {
+            InitializeComponent();
+        }
+        
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            Form2.ControlBox = false;
+            rdoErkek.Checked = false;
+            rdoBayan.Checked = false;
+            t.Location = new Point(40, 40);
+            Label l = new Label();
+            l.Location = new Point(40,20);
+            l.Text = "Koltuk Sayısı :";
+            Button b = new Button();
+            b.Text = "ÇİZ";
+            b.Location = new Point(40,60);
+            b.Click+=new EventHandler(b_Click);
+            Form2.Controls.Add(t);
+            Form2.Controls.Add(l);
+            Form2.Controls.Add(b);
+            Form2.ShowDialog();
+            grpMusteri.Visible = false;
+            grpYolculistesi.Visible = false;
+
+
+            for (int i = 0; i < 40; i++)
+            {
+                yolcular[i] = "BOŞ";
+            }
+        }
+        void b_Click(object sender, EventArgs e)
+        {
+           
+            string sayi=t.Text;
+            bool devam = false;
+
+            for (int i = 0; i < sayi.Length; i++)
+		    {
+                 if (!char.IsDigit(sayi[i]))
+                    devam = true;
+		    }
+
+            if (!devam&&sayi.Length!=0)
+            {
+                if (int.Parse(t.Text) <= 40 &&int.Parse(t.Text)>0)
+                {
+                    koltukSayisi = int.Parse(t.Text);
+                    otobusCiz(koltukSayisi);
+                    Form2.Close();
+                    for (int i = 0; i < koltukSayisi; i++)
+                    {
+                       koltukcins[i]=" ";
+                    }
+                }
+                else
+                    MessageBox.Show("lütfen 40 veya 40 tan küçük sayi giriniz");
+            }
+            else
+                MessageBox.Show("lütfen 40 veya 40 tan küçük sayi giriniz");
+           
+            
+            t.Text = "";
+        }
+
+        void otobusCiz(int koltukSayisi)
+        {
+            
+            int sutun = 0,b=0,satir = 0;;
+
+            for (int i = 0; i < koltukSayisi; i++)
+            {
+                if (i % 4 == 0 && i != 0)
+                {
+                    satir++;
+                    sutun = 0;
+                    b = 0;
+                }
+                else
+                    if (i % 2 == 0 && i!=0)
+                        b = 40;
+                Button koltuk = new Button();
+                koltuk.Name = i.ToString();
+                koltuk.TabIndex = i;
+                koltuk.TabStop = false;
+                koltuk.Text = (i+1).ToString();
+                koltuk.Size = new System.Drawing.Size(30, 30);
+                Point p = new System.Drawing.Point(30+sutun * koltuk.Width + sutun*10+b, 30+ satir * koltuk.Height*3/2);
+                koltuk.Location = p;
+                koltuk.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+                koltuk.Visible = true;
+                koltuk.BackColor = Color.Gray;
+                koltuk.BringToFront();
+                koltuk.MouseDown += new MouseEventHandler(this.koltuklarClick);
+                grpbkoltuklar.Controls.Add(koltuk);
+                this.AutoSize = true;
+                koltuklar.Add(koltuk);
+                sutun++;
+            }
+
+            satir += koltukSayisi / 8;
+            grpbkoltuklar.Size = new Size(250, (int)satir * 30+ 100);
+        }
+
+
+        private void koltuklarClick(object sender, System.Windows.Forms.MouseEventArgs e)
+        {
+            BasılanButon = (Button)sender;
+
+            grpYolculistesi.Visible = false;
+            
+            grpMusteri.Visible = true;
+            
+
+        }
+
+        private void btnTamam_Click(object sender, EventArgs e)
+        {
+            int koltukNo=int.Parse(BasılanButon.Text);
+            string cinsiyet = "";
+
+
+            if (rdoBayan.Checked == true)
+            {
+                cinsiyet = "Bayan";
+                koltukcins[koltukNo] = "B";
+            }
+            else
+                if(rdoErkek.Checked == true)
+                {
+                    cinsiyet = "Erkek";
+                    koltukcins[koltukNo] = "E";
+                }
+
+
+
+            BasılanButon.BackColor = Color.Red;
+            BasılanButon.Enabled = false;
+            yolcular[koltukNo-1]=txtAd.Text.PadRight(12,' ')+txtSoyad.Text.PadRight(12,' ')+cinsiyet.PadRight(8,' ');
+            yolcuSayisi++;
+            grpMusteri.Visible = false;
+            txtAd.Text = "";
+            txtSoyad.Text = "";
+            rdoBayan.Checked = false;
+            rdoErkek.Checked = false;
+            this.Text = "Yolcu sayisi = " + yolcuSayisi;
+
+        }
+
+        private void btnIptal_Click(object sender, EventArgs e)
+        {
+            grpMusteri.Visible = false;
+            txtAd.Text = "";
+            txtSoyad.Text = "";
+            BasılanButon.BackColor = Color.Gray;
+            BasılanButon.Enabled = true;
+        }
+
+        private void btnGoster_Click(object sender, EventArgs e)
+        {
+            lstYolculiste.Items.Clear();
+            grpYolculistesi.Visible = true;
+            grpYolculistesi.Location = new Point(350, 40);
+
+
+            for (int i = 0; i < koltukSayisi; i++)
+            {
+                lstYolculiste.Items.Add((i+1)+". koltuk:  \t"+yolcular[i]);
+            }
+        }
+
+        private void rdoBayan_CheckedChanged(object sender, EventArgs e)
+        {
+            int koltukNo = int.Parse(BasılanButon.Text);
+
+
+            if (rdoBayan.Checked == true)
+            {
+                koltukcins[koltukNo] = "B";
+            }
+            else if (rdoErkek.Checked == true)
+            {
+                koltukcins[koltukNo] = "E";
+            }
+
+            int a = koltukNo;
+
+            if (koltukNo % 4 == 0)
+                a--;
+            if (koltukNo % 4 == 1)
+                a++;
+            if (koltukNo % 4 == 2)
+                a--;
+            if (koltukNo % 4 == 3)
+                a++;
+
+            if (koltukcins[a] == "B")
+            {
+                if (koltukcins[koltukNo] == "E")
+                {
+                    MessageBox.Show("Bu koltuga sadece bayan biri oturabilir");
+                    btnIptal_Click(sender, e);
+                }
+            }
+        }
+
+        private void rdoErkek_CheckedChanged(object sender, EventArgs e)
+        {
+            rdoBayan_CheckedChanged(sender, e);
+        }
+
+    }
+}
